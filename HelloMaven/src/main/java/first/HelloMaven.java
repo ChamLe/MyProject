5852cd899c9b97579e7c186fd85dfafaa99070ed
Chamle <chamle1602@gmail.com>
2020-03-03 16:44:12 +0800

    Create project

---
+++ HelloMaven/src/main/java/first/HelloMaven.java
@@ -0,0 +1,12 @@
+package first;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.isNumeric("123"));
+
+	}
+
+}

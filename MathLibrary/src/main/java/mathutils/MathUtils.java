97c3f1f2f030b6246ad447421029c3c3ecc9f78e
Chamle <chamle1602@gmail.com>
2020-03-06 15:37:13 +0800

    Module word together

---
+++ MathLibrary/src/main/java/mathutils/MathUtils.java
@@ -0,0 +1,14 @@
+package mathutils;
+
+public class MathUtils {
+	/**
+	 * Sum of 2 integers
+	 * @param a integer
+	 * @param b integer
+	 * @return integer
+	 */
+	public static int sum(int a, int b){
+		return a+b;
+	}
+
+}
---
+++ MathWebApp/src/main/java/Test/test.java
@@ -0,0 +1,12 @@
+package Test;
+
+import mathutils.MathUtils;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println(MathUtils.sum(4, 4));
+
+	}
+
+}
